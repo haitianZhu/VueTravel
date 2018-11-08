4607f24de4f79e815228710246ba8e07609b811f
haitian <nova_zhuht@126.com>
2018-11-08 14:15:32 +0800

    1.city list 2.better-scroll

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">C</li>
+    <li class="item">D</li>
+    <li class="item">E</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~@/assets/styles/varibles.styl"
+  .list
+    display flex
+    flex-direction column
+    justify-content center
+    position absolute
+    width .4rem
+    top 1.58rem
+    right 0
+    bottom 0
+    .item
+      line-height .44rem
+      text-align center
+      color $bgColor
+</style>
